b4c965ef18a2e99a220b59cd35ba923ba7a6c198
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-12-03 20:46:47 -0300

    initial structure folder, litte update readme

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./Button.scss"
+
+const Button = () => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./SideBar.scss"
+
+const SideBar = () => {
+  return (
+    <div>SideBar</div>
+  )
+}
+
+export default SideBar
