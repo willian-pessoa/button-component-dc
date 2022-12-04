685270f6f072f36a2094bbdf1f37c104ececd293
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-12-04 02:24:24 -0300

    button default, outline and text add

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import "./Button.scss"
+import "./Button.scss";
 
-const Button = () => {
+const Button = (props) => {
+  console.log(props.disabled)
+
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      className={`btn btn-${props.variant || "default"} ${
+        props.hover && props.hover
+      } ${props.disableShadow && "disableShadow"}`}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from "react";
 
-import "./Main.scss"
+import Button from "../Button/Button";
+
+import "./Main.scss";
 
 const Main = () => {
   return (
-    <main className='main'>Main</main>
-  )
-}
+    <main className="main">
+      <h3>Buttons</h3>
+      <div className="main-variants">
+        <div className="row-variant">
+          <div className="cel-variant">
+            <span className="describe-btn">{"<Button />"}</span>
+            <Button>Default</Button>
+          </div>
+          <div className="cel-variant">
+            <span className="describe-btn">{"&:hover, &:focus"}</span>
+            <Button hover="hover-default">Default</Button>
+          </div>
+        </div>
+        <div className="row-variant">
+          <div className="cel-variant">
+            <span className="describe-btn">
+              {'<Button variant="outline" />'}
+            </span>
+            <Button variant="outline">Default</Button>
+          </div>
+          <div className="cel-variant">
+            <span className="describe-btn">{"&:hover, &:focus"}</span>
+            <Button hover="hover-outline" variant="outline">
+              Default
+            </Button>
+          </div>
+        </div>
+        <div className="row-variant">
+          <div className="cel-variant">
+            <span className="describe-btn">{'<Button variant="text" />'}</span>
+            <Button variant="text">Default</Button>
+          </div>
+          <div className="cel-variant">
+            <span className="describe-btn">{"&:hover, &:focus"}</span>
+            <Button hover="hover-text" variant="text">
+              Default
+            </Button>
+          </div>
+        </div>
+        <div className="row-variant">
+          <div className="cel-variant">
+            <span className="describe-btn">{"<Button disableShadow />"}</span>
+            <Button disableShadow>Default</Button>
+          </div>
+        </div>
+        <div className="row-variant">
+          <div className="cel-variant">
+            <span className="describe-btn">{"<Button disabled />"}</span>
+            <Button disabled>Default</Button>
+          </div>
+          <div className="cel-variant">
+            <span className="describe-btn">
+              {'<Button variant="text" disabled />'}
+            </span>
+            <Button variant="text" disabled>
+              Default
+            </Button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Main
+export default Main;
